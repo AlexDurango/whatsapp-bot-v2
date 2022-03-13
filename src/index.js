a1158a2d5725f3bdfd267418f3c47f0a24378de1
AlexDurango <alex.durango2303@gmail.com>
2022-03-12 21:26:10 -0500

    Fifth commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@
 //const qrcode = require("qrcode-terminal");
 const { Client, LocalAuth } = require("whatsapp-web.js");
 
+// Cargar el modulo HTTP
+var http = require('http');
+// Configurar una respuesta HTTP para todas las peticiones
+function onRequest(request, response) {
+  console.log("Peticion Recibida.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hola Mundo");
+  response.end();
+}
+var server = http.createServer(onRequest);
+// Escuchar al puerto 8080
+server.listen(process.env.PORT);
+
 const country_code = '57';
 const number = '3135310417';
 const identificador = '@c.us'
