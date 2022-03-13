f1a46ac60750a9b18ea821320d79763de43b6eb3
AlexDurango <alex.durango2303@gmail.com>
2022-03-12 20:36:27 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,52 @@
+// Librerias
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
+
+const country_code = '57';
+const number = '3135310417';
+const identificador = '@c.us'
+
+const client = new Client({
+    authStrategy: new LocalAuth({
+        clientId: 'client-on'
+    })
+});
+
+client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+
+client.on("message",  message => {
+
+    let idMessage = message.from
+
+    if(message.body == 'Servidor') {
+        client.sendMessage(idMessage, 'En correcto funcionamiento')
+    }
+    
+} );
+
+function main () {
+    let date = new Date()
+
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+
+    //console.log(minutes)
+
+    if (minutes == 0) {
+        client.sendMessage(`${country_code}${number}${identificador}`, `Son las ${hours}:${minutes}, guapo`)
+    }
+
+}
+
+client.on("ready", () => {
+    console.log('Working')
+
+    setInterval(main, 59999)
+})
+
+
+
+client.initialize();
